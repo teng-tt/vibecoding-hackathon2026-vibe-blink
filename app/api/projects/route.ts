16c76e282e7623fa8514fafe5f9180a84415ce7c
teng-tt <tengminghong199619@126.com>
2026-01-29 13:54:28 +0800

    fix debug

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -3,12 +3,6 @@ import { addProject, getAllProjects, getProjectById, deleteProject, updateProjec
 
 export async function GET(request: NextRequest) {
   try {
-        console.log("DB_URL是否存在1:", !!process.env.POSTGRES_URL); 
-        console.log("DB_URL是否存在2:", !!process.env.POSTGRES_URL_NON_POOLING); 
-        console.log("DB_URL是否存在1:", process.env.POSTGRES_URL); 
-        console.log("DB_URL是否存在2:", process.env.POSTGRES_URL_NON_POOLING); 
-         console.log("Server Side Check1:", process.env.POSTGRES_URL ? "Has Value" : "No Value");
-          console.log("Server Side Check2:", process.env.POSTGRES_URL_NON_POOLING ? "Has Value" : "No Value");
     const { searchParams } = new URL(request.url);
     const id = searchParams.get("id");
 
@@ -39,6 +33,12 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   try {
+            console.log("DB_URL是否存在1:", !!process.env.POSTGRES_URL); 
+        console.log("DB_URL是否存在2:", !!process.env.POSTGRES_URL_NON_POOLING); 
+        console.log("DB_URL是否存在1:", process.env.POSTGRES_URL); 
+        console.log("DB_URL是否存在2:", process.env.POSTGRES_URL_NON_POOLING); 
+         console.log("Server Side Check1:", process.env.POSTGRES_URL ? "Has Value" : "No Value");
+          console.log("Server Side Check2:", process.env.POSTGRES_URL_NON_POOLING ? "Has Value" : "No Value");
     const body = await request.json();
     
     const project: Project = {
